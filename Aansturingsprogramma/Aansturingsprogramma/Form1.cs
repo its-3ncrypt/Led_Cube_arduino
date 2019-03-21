c5a72fe206c01919f3c8f97302dcc4515c130cbb
3ncrypt <sebastiaan.sillis@gmail.com>
2019-03-21 14:18:07 +0100

    implemented outputting code

--- Aansturingsprogramma/Aansturingsprogramma/Form1.cs
+++ Aansturingsprogramma/Aansturingsprogramma/Form1.cs
@@ -27,7 +27,6 @@ namespace Aansturingsprogramma
         bool Ledraster6 = false;
         bool Ledraster7 = false;
         bool IsFrozen = false;
-        string sentCode;
         int[,] ledGen = new int[20,512];
         int EffectNumber = 0;
         int Snelheid = 2;
@@ -41,7 +40,7 @@ namespace Aansturingsprogramma
         public Form1()
         {
             InitializeComponent();
-            //disableControls();
+            disableControls();
             getAvailableComPorts();
             EmptyCode(0);
             //poorten zichtbaar maken
@@ -648,6 +647,140 @@ namespace Aansturingsprogramma
         {
             Upload();
         }
+        private static string Printout(int[,] tested)
+        {
+            int[] test1 = new int[512];
+            int[] test2 = new int[512];
+            int[] test3 = new int[512];
+            int[] test4 = new int[512];
+            int[] test5 = new int[512];
+            int[] test6 = new int[512];
+            int[] test7 = new int[512];
+            int[] test8 = new int[512];
+            int[] test9 = new int[512];
+            int[] test10 = new int[512];
+            int[] test11 = new int[512];
+            int[] test12 = new int[512];
+            int[] test13 = new int[512];
+            int[] test14 = new int[512];
+            int[] test15 = new int[512];
+            int[] test16 = new int[512];
+            int[] test17 = new int[512];
+            int[] test18 = new int[512];
+            int[] test19 = new int[512];
+            int[] test20 = new int[512];
+            string testString1;
+            string testString2;
+            string testString3;
+            string testString4;
+            string testString5;
+            string testString6;
+            string testString7;
+            string testString8;
+            string testString9;
+            string testString10;
+            string testString11;
+            string testString12;
+            string testString13;
+            string testString14;
+            string testString15;
+            string testString16;
+            string testString17;
+            string testString18;
+            string testString19;
+            string testString20;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int y = 0; y < 512; y++)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            test1[y] = tested[i, y];
+                            break;
+                        case 1:
+                            test2[y] = tested[i, y];
+                            break;
+                        case 2:
+                            test3[y] = tested[i, y];
+                            break;
+                        case 3:
+                            test4[y] = tested[i, y];
+                            break;
+                        case 4:
+                            test5[y] = tested[i, y];
+                            break;
+                        case 5:
+                            test6[y] = tested[i, y];
+                            break;
+                        case 6:
+                            test7[y] = tested[i, y];
+                            break;
+                        case 7:
+                            test8[y] = tested[i, y];
+                            break;
+                        case 8:
+                            test9[y] = tested[i, y];
+                            break;
+                        case 9:
+                            test10[y] = tested[i, y];
+                            break;
+                        case 10:
+                            test11[y] = tested[i, y];
+                            break;
+                        case 11:
+                            test12[y] = tested[i, y];
+                            break;
+                        case 12:
+                            test13[y] = tested[i, y];
+                            break;
+                        case 13:
+                            test14[y] = tested[i, y];
+                            break;
+                        case 14:
+                            test15[y] = tested[i, y];
+                            break;
+                        case 15:
+                            test16[y] = tested[i, y];
+                            break;
+                        case 16:
+                            test17[y] = tested[i, y];
+                            break;
+                        case 17:
+                            test18[y] = tested[i, y];
+                            break;
+                        case 18:
+                            test19[y] = tested[i, y];
+                            break;
+                        case 19:
+                            test20[y] = tested[i, y];
+                            break;
+                    }
+                }
+            }
+            testString1 = String.Join("", test1);
+            testString2 = String.Join("", test2);
+            testString3 = String.Join("", test3);
+            testString4 = String.Join("", test4);
+            testString5 = String.Join("", test5);
+            testString6 = String.Join("", test6);
+            testString7 = String.Join("", test7);
+            testString8 = String.Join("", test8);
+            testString9 = String.Join("", test9);
+            testString10 = String.Join("", test10);
+            testString11 = String.Join("", test11);
+            testString12 = String.Join("", test12);
+            testString13 = String.Join("", test13);
+            testString14 = String.Join("", test14);
+            testString15 = String.Join("", test15);
+            testString16 = String.Join("", test16);
+            testString17 = String.Join("", test17);
+            testString18 = String.Join("", test18);
+            testString19 = String.Join("", test19);
+            testString20 = String.Join("", test20);
+
+            return (testString1 + testString2 + testString3 + testString4 + testString5 + testString6 + testString7 + testString8 + testString9 + testString10 + testString11 + testString12 + testString13 + testString14 + testString15 + testString16 + testString17 + testString18 + testString19 + testString20);
+        }
 
 
         //Upload algoritme
@@ -659,21 +792,7 @@ namespace Aansturingsprogramma
                 {
                     port.Write(Convert.ToString(Snelheid));
                     port.Write(Convert.ToString(EffectNumber));
-                    for (int i = 0; i <= 19; i++)
-                    {
-                        if (IsSet[i] == false)
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            for (int y = 0; y < 512; y++)
-                            {
-                                sentCode += Convert.ToString(ledGen[i, y]);
-                            }
-                        }
-                    }
-                    port.Write(sentCode);
+                    port.Write(Printout(ledGen));
                     
                 }
                 else
--- test code converter/test code converter/Program.cs
+++ test code converter/test code converter/Program.cs
@@ -10,11 +10,11 @@ namespace test_code_converter
             int[,] converted = new int[20, 512];
 
             //code genereren
-            for (int i = 0;i<20;i++)
+            for (int i = 0; i < 20; i++)
             {
                 for (int y = 0; y < 512; y++)
                 {
-                    if(y %2 != 0)
+                    if (y % 2 != 0)
                     {
                         test[i, y] = 1;
                     }
@@ -27,140 +27,236 @@ namespace test_code_converter
             Console.WriteLine(Printout(test));
             Console.ReadLine();
         }
-        //resultaten uitprinten
-        static string Printout(int[,] tested)
+        private static int[,] ConverterToArduinoCode(int[,] tested1)
         {
-            int[] test1 = new int[512];
-            int[] test2 = new int[512];
-            int[] test3 = new int[512];
-            int[] test4 = new int[512];
-            int[] test5 = new int[512];
-            int[] test6 = new int[512];
-            int[] test7 = new int[512];
-            int[] test8 = new int[512];
-            int[] test9 = new int[512];
-            int[] test10 = new int[512];
-            int[] test11 = new int[512];
-            int[] test12 = new int[512];
-            int[] test13 = new int[512];
-            int[] test14 = new int[512];
-            int[] test15 = new int[512];
-            int[] test16 = new int[512];
-            int[] test17 = new int[512];
-            int[] test18 = new int[512];
-            int[] test19 = new int[512];
-            int[] test20 = new int[512];
-            string testString1;
-            string testString2;
-            string testString3;
-            string testString4;
-            string testString5;
-            string testString6;
-            string testString7;
-            string testString8;
-            string testString9;
-            string testString10;
-            string testString11;
-            string testString12;
-            string testString13;
-            string testString14;
-            string testString15;
-            string testString16;
-            string testString17;
-            string testString18;
-            string testString19;
-            string testString20;
+            int[,] fixeda = new int[20, 512];
+            int[] test1111 = new int[512];
+            int[] test211 = new int[512];
+            int[] test311 = new int[512];
+            int[] test411 = new int[512];
+            int[] test511 = new int[512];
+            int[] test611 = new int[512];
+            int[] test711 = new int[512];
+            int[] test811 = new int[512];
+            int[] test911 = new int[512];
+            int[] test101 = new int[512];
+            int[] test111 = new int[512];
+            int[] test121 = new int[512];
+            int[] test131 = new int[512];
+            int[] test141 = new int[512];
+            int[] test151 = new int[512];
+            int[] test161 = new int[512];
+            int[] test171 = new int[512];
+            int[] test181 = new int[512];
+            int[] test191 = new int[512];
+            int[] test201 = new int[512];
+
             for (int i = 0; i < 20; i++)
             {
-                for(int y = 0; y < 512; y++)
+                for (int y = 0; y < 512; y++)
                 {
                     switch (i)
                     {
                         case 0:
-                            test1[y] = tested[i, y];
+                            test1111[y] = tested1[i, y];
                             break;
                         case 1:
-                            test2[y] = tested[i, y];
+                            test211[y] = tested1[i, y];
                             break;
                         case 2:
-                            test3[y] = tested[i, y];
+                            test311[y] = tested1[i, y];
                             break;
                         case 3:
-                            test4[y] = tested[i, y];
+                            test411[y] = tested1[i, y];
                             break;
                         case 4:
-                            test5[y] = tested[i, y];
+                            test511[y] = tested1[i, y];
                             break;
                         case 5:
-                            test6[y] = tested[i, y];
+                            test611[y] = tested1[i, y];
                             break;
                         case 6:
-                            test7[y] = tested[i, y];
+                            test711[y] = tested1[i, y];
                             break;
                         case 7:
-                            test8[y] = tested[i, y];
+                            test811[y] = tested1[i, y];
                             break;
                         case 8:
-                            test9[y] = tested[i, y];
+                            test911[y] = tested1[i, y];
                             break;
                         case 9:
-                            test10[y] = tested[i, y];
+                            test101[y] = tested1[i, y];
                             break;
                         case 10:
-                            test11[y] = tested[i, y];
+                            test111[y] = tested1[i, y];
                             break;
                         case 11:
-                            test12[y] = tested[i, y];
+                            test121[y] = tested1[i, y];
                             break;
                         case 12:
-                            test13[y] = tested[i, y];
+                            test131[y] = tested1[i, y];
                             break;
                         case 13:
-                            test14[y] = tested[i, y];
+                            test141[y] = tested1[i, y];
                             break;
                         case 14:
-                            test15[y] = tested[i, y];
+                            test151[y] = tested1[i, y];
                             break;
                         case 15:
-                            test16[y] = tested[i, y];
+                            test161[y] = tested1[i, y];
                             break;
                         case 16:
-                            test17[y] = tested[i, y];
+                            test171[y] = tested1[i, y];
                             break;
                         case 17:
-                            test18[y] = tested[i, y];
+                            test181[y] = tested1[i, y];
                             break;
                         case 18:
-                            test19[y] = tested[i, y];
+                            test191[y] = tested1[i, y];
                             break;
                         case 19:
-                            test20[y] = tested[i, y];
+                            test201[y] = tested1[i, y];
                             break;
                     }
                 }
-            }
-            testString1 = String.Join("", test1);
-            testString2 = String.Join("", test2);
-            testString3 = String.Join("", test3);
-            testString4 = String.Join("", test4);
-            testString5 = String.Join("", test5);
-            testString6 = String.Join("", test6);
-            testString7 = String.Join("", test7);
-            testString8 = String.Join("", test8);
-            testString9 = String.Join("", test9);
-            testString10 = String.Join("", test10);
-            testString11 = String.Join("", test11);
-            testString12 = String.Join("", test12);
-            testString13 = String.Join("", test13);
-            testString14 = String.Join("", test14);
-            testString15 = String.Join("", test15);
-            testString16 = String.Join("", test16);
-            testString17 = String.Join("", test17);
-            testString18 = String.Join("", test18);
-            testString19 = String.Join("", test19);
-            testString20 = String.Join("", test20);
 
-            return(testString1+testString2+testString3+testString4+testString5+testString6+testString7+testString8+testString9+testString10+testString11+testString12+testString13+testString14+testString15+testString16+testString17+testString18+testString19+testString20) ;
+                return fixeda;
+            }
+        }
+            //resultaten uitprinten
+            private static string Printout(int[,] tested)
+            {
+                int[] test1 = new int[512];
+                int[] test2 = new int[512];
+                int[] test3 = new int[512];
+                int[] test4 = new int[512];
+                int[] test5 = new int[512];
+                int[] test6 = new int[512];
+                int[] test7 = new int[512];
+                int[] test8 = new int[512];
+                int[] test9 = new int[512];
+                int[] test10 = new int[512];
+                int[] test11 = new int[512];
+                int[] test12 = new int[512];
+                int[] test13 = new int[512];
+                int[] test14 = new int[512];
+                int[] test15 = new int[512];
+                int[] test16 = new int[512];
+                int[] test17 = new int[512];
+                int[] test18 = new int[512];
+                int[] test19 = new int[512];
+                int[] test20 = new int[512];
+                string testString1;
+                string testString2;
+                string testString3;
+                string testString4;
+                string testString5;
+                string testString6;
+                string testString7;
+                string testString8;
+                string testString9;
+                string testString10;
+                string testString11;
+                string testString12;
+                string testString13;
+                string testString14;
+                string testString15;
+                string testString16;
+                string testString17;
+                string testString18;
+                string testString19;
+                string testString20;
+                for (int i = 0; i < 20; i++)
+                {
+                    for (int y = 0; y < 512; y++)
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                test1[y] = tested[i, y];
+                                break;
+                            case 1:
+                                test2[y] = tested[i, y];
+                                break;
+                            case 2:
+                                test3[y] = tested[i, y];
+                                break;
+                            case 3:
+                                test4[y] = tested[i, y];
+                                break;
+                            case 4:
+                                test5[y] = tested[i, y];
+                                break;
+                            case 5:
+                                test6[y] = tested[i, y];
+                                break;
+                            case 6:
+                                test7[y] = tested[i, y];
+                                break;
+                            case 7:
+                                test8[y] = tested[i, y];
+                                break;
+                            case 8:
+                                test9[y] = tested[i, y];
+                                break;
+                            case 9:
+                                test10[y] = tested[i, y];
+                                break;
+                            case 10:
+                                test11[y] = tested[i, y];
+                                break;
+                            case 11:
+                                test12[y] = tested[i, y];
+                                break;
+                            case 12:
+                                test13[y] = tested[i, y];
+                                break;
+                            case 13:
+                                test14[y] = tested[i, y];
+                                break;
+                            case 14:
+                                test15[y] = tested[i, y];
+                                break;
+                            case 15:
+                                test16[y] = tested[i, y];
+                                break;
+                            case 16:
+                                test17[y] = tested[i, y];
+                                break;
+                            case 17:
+                                test18[y] = tested[i, y];
+                                break;
+                            case 18:
+                                test19[y] = tested[i, y];
+                                break;
+                            case 19:
+                                test20[y] = tested[i, y];
+                                break;
+                        }
+                    }
+                }
+                testString1 = String.Join("", test1);
+                testString2 = String.Join("", test2);
+                testString3 = String.Join("", test3);
+                testString4 = String.Join("", test4);
+                testString5 = String.Join("", test5);
+                testString6 = String.Join("", test6);
+                testString7 = String.Join("", test7);
+                testString8 = String.Join("", test8);
+                testString9 = String.Join("", test9);
+                testString10 = String.Join("", test10);
+                testString11 = String.Join("", test11);
+                testString12 = String.Join("", test12);
+                testString13 = String.Join("", test13);
+                testString14 = String.Join("", test14);
+                testString15 = String.Join("", test15);
+                testString16 = String.Join("", test16);
+                testString17 = String.Join("", test17);
+                testString18 = String.Join("", test18);
+                testString19 = String.Join("", test19);
+                testString20 = String.Join("", test20);
+
+                return (testString1 + testString2 + testString3 + testString4 + testString5 + testString6 + testString7 + testString8 + testString9 + testString10 + testString11 + testString12 + testString13 + testString14 + testString15 + testString16 + testString17 + testString18 + testString19 + testString20);
+            }
         }
     }
-}
